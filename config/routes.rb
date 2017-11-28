343f1a32df51b6afc0820b560dce51b8dc353213
Natalya Buchwald <nbuchwal@andrew.cmu.edu>
2017-11-28 11:42:14 -0500

    generated home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  # Semi-static page routes
+  get 'home', to: 'home#index', as: :home
+
   resources :users
   resources :saved_articles
   resources :ratings
   resources :locations
   resources :articles
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
